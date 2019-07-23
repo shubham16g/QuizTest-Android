4eed95523d4fc75bedf571731a17fb61fa031ac4
Shubham Gupta <shubham.developer16@gmail.com>
2019-07-23 22:11:56 +0530

    Logo fixed

--- app/src/main/java/test/quiz/com/quiztest/MainMenuActivity.java
+++ app/src/main/java/test/quiz/com/quiztest/MainMenuActivity.java
@@ -2,13 +2,19 @@ package test.quiz.com.quiztest;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import java.util.Random;
 
 public class MainMenuActivity extends AppCompatActivity {
+
+    Button instnatQuiz,exit;
+    String [] serverQuestions;
+    SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
